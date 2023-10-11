0232894139bb73973b904d6b97dfc66ca836f53a
Yura Lee <pie514@naver.com>
2023-10-12 00:50:11 +0900

    update-231012_03-commit

--- js/common.js
+++ js/common.js
@@ -9,11 +9,14 @@ bannerClose.addEventListener('click', () => {
 const header = document.querySelector('#header-area');
 const btnMenu = document.querySelector('.btn-menu');
 const btnClose = document.querySelector('.btn-close');
+const body = document.querySelector('body')
 
 btnMenu.addEventListener('click', ()=> {
-  header.classList.add('active')
+  header.classList.add('active');
+  body.classList.add('scroll-hidden');
 })
 btnClose.addEventListener('click', ()=> {
   header.classList.remove('active')
+  body.classList.remove('scroll-hidden');
 })
 
